99ef7922d202f23e1f3a5b5e87a06e778609486d
statpileelkhayate <melkhayate61@gmail.com>
2025-06-10 13:49:49 +0200

    Chore: adjust roles

--- src/routes/containers.ts
+++ src/routes/containers.ts
@@ -9,8 +9,8 @@ const router = Router();
 router.use(authenticate as RequestHandler);
 
 router.get("/", authorize(["admin", "viewer", "uploader"]) as RequestHandler, getContainers as unknown as RequestHandler);
-router.post("/", authorize(["admin"]) as RequestHandler, createContainer as unknown as RequestHandler);
-router.put("/:containerName", authorize(["admin"]) as RequestHandler, updateContainer as unknown as RequestHandler);
-router.delete("/:containerName", authorize(["admin"]) as RequestHandler, deleteContainer as unknown as RequestHandler);
+router.post("/", authorize(["admin", "uploader"]) as RequestHandler, createContainer as unknown as RequestHandler);
+router.put("/:containerName", authorize(["admin", "uploader"]) as RequestHandler, updateContainer as unknown as RequestHandler);
+router.delete("/:containerName", authorize(["admin", "uploader"]) as RequestHandler, deleteContainer as unknown as RequestHandler);
 
 export default router;
